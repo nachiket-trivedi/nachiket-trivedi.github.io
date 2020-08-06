626e0e423ecf22ae7b3625c38242844150d13692
Nachiket Trivedi <nachiket5197@gmail.com>
2020-08-05 19:14:55 -0700

    Updates

---
+++ precache-manifest.9a17da87631f38cac7825fd4f530aa3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae3036e48095384ada8612ba30703635",
+    "url": "/index.html"
+  },
+  {
+    "revision": "911f2a13f4f999f067f1",
+    "url": "/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "ea4330bf4e256feb0be1",
+    "url": "/static/css/main.df4f50d4.chunk.css"
+  },
+  {
+    "revision": "911f2a13f4f999f067f1",
+    "url": "/static/js/2.85976dee.chunk.js"
+  },
+  {
+    "revision": "9683930def76adeece1eb4e95fd24386",
+    "url": "/static/js/2.85976dee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea4330bf4e256feb0be1",
+    "url": "/static/js/main.c9235754.chunk.js"
+  },
+  {
+    "revision": "14d028a6a9d3bd8ed145",
+    "url": "/static/js/runtime-main.f4821ffc.js"
+  },
+  {
+    "revision": "a30dd618c985d86ffb7e5c5f1504ee7f",
+    "url": "/static/media/resume.a30dd618.pdf"
+  }
+]);
